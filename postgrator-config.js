63324499c66524c63203754c53df9dde0774c80e
Madison Slansky <madisonslansky@Madisons-Air.attlocal.net>
2021-02-12 12:58:57 -0600

    Postgrator not found

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,5 +4,6 @@ const result = require('dotenv').config();
 module.exports = {
   'migrationDirectory': 'migrations',
   'driver': 'pg',
-  'connectionString': process.env.DB_URL
+  'connectionString': process.env.DB_URL,
+  'ssl': process.env.MIGRATION_SSL
 };
